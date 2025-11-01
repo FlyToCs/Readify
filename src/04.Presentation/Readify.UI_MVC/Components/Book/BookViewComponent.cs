f84881ca4c6eff69bfa662f9b62575a7e2bfc330
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2025-11-01 22:47:01 +0330

    move book and category section to viewComponents

---
+++ src/04.Presentation/Readify.UI_MVC/Components/Book/BookViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Readify.Domain.BookAgg.Contracts.ServiceContracts;
+
+namespace Readify.UI_MVC.Components.Book
+{
+    public class BookViewComponent(IBookService bookService) : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var books = bookService.GetRecentlyBooks(5);
+            return View("Books", books);
+        }
+    }
+}
---
+++ src/04.Presentation/Readify.UI_MVC/Components/Category/CategoryViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Readify.Domain.CategoryAgg.Contracts.ServiceContracts;
+using Readify.Domain.CategoryAgg.DTOs;
+using Readify.Services;
+
+namespace Readify.UI_MVC.Components.Category
+{
+    public class CategoryViewComponent(ICategoryService categoryService) : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var categories = categoryService.GetPopularCategories(5);
+            return View("Categories", categories);
+        }
+    }
+}
