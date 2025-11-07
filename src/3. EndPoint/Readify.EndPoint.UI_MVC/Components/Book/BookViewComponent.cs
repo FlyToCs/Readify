919c54bbfc05cb5213c656f54f745b82bf831a81
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2025-11-07 15:46:58 +0330

    update namespaces

---
+++ src/3. EndPoint/Readify.EndPoint.UI_MVC/Components/Book/BookViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Readify.Domain.Core.Book.Services;
+
+namespace Readify.EndPoint.UI_MVC.Components.Book
+{
+    public class BookViewComponent(IBookService bookService) : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var books = bookService.GetRecentlyBooks(5);
+            return View("Books", books);
+        }
+    }
+}
---
+++ src/3. EndPoint/Readify.EndPoint.UI_MVC/Components/Category/CategoryViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Readify.Domain.Core.Category.Services;
+
+namespace Readify.EndPoint.UI_MVC.Components.Category
+{
+    public class CategoryViewComponent(ICategoryService categoryService) : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var categories = categoryService.GetPopularCategories(5);
+            return View("Categories", categories);
+        }
+    }
+}
